7353d783b2cc9bcd54be9d34d79ac8931982fcac
Nayanjyoti Das <102731924+nayan099@users.noreply.github.com>
2022-11-28 00:52:23 +0530

    Update notes.php

--- notes.php
+++ notes.php
@@ -243,15 +243,12 @@ else{
     edits = document.getElementsByClassName('edit');
     Array.from(edits).forEach((element) => {
       element.addEventListener("click", (e) => {
-        console.log("edit ");
         tr = e.target.parentNode.parentNode;
         title = tr.getElementsByTagName("td")[0].innerText;
         description = tr.getElementsByTagName("td")[1].innerText;
-        console.log(title, description);
         titleEdit.value = title;
         descriptionEdit.value = description;
         snoEdit.value = e.target.id;
-        console.log(e.target.id)
         $('#editModal').modal('toggle');
       })
     })
@@ -259,16 +256,11 @@ else{
     deletes = document.getElementsByClassName('delete');
     Array.from(deletes).forEach((element) => {
       element.addEventListener("click", (e) => {
-        console.log("edit ");
         sno = e.target.id.substr(1);
 
         if (confirm("Are you sure you want to delete this note!")) {
-          console.log("yes");
           window.location = `/tuts/notes.php?delete=${sno}`;
 
-        }
-        else {
-          console.log("no");
         }
       })
     })
